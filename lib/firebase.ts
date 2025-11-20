b4fea5e2dae383702d6dcc86a41e92a3035bfbe0
montasirmoyen <montasirmoyen@gmail.com>
2025-11-19 20:18:34 -0500

    Re-init firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,12 +10,12 @@ if (!process.env.NEXT_PUBLIC_FIREBASE_API_KEY) {
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "luxtra-ead96.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "luxtra-ead96",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "luxtra-ead96.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "521836196224",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:521836196224:web:7c3a5607f25cd49a6dd165",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-KY1W9GK3YT"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
