91cb5c415edf2b8d9f41aa8c0595617f52abcfa8
montasirmoyen <montasirmoyen@gmail.com>
2025-11-19 19:58:36 -0500

    Update layout, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "../contexts/auth-context";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "LuxTra",
-  description: "Luxury Transportation Service",
+  title: "Boston Luxury Express | Luxury Transportation",
+  description: "Luxury Transportation",
 };
 
 export default function RootLayout({
@@ -25,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
